879f6639f2e60bfd6beda4a36f905ce8e587de80
Bent André Solheim <bent.andre.solheim@kodehuset.no>
2017-09-20 09:14:30 +0200

    Feature/aos 1659 replace left shift with dolast (#1)
    * Gradle wrapper refresh
    
    * Upgraded to gradle 4.1
    
    * Replaced left shift with doLast
    
    * Using source distro
    
    * aurora-gradle-plugin:1.2.1

--- src/main/groovy/no/skatteetaten/aurora/gradle/plugins/docker/AuroraDockerPlugin.groovy
+++ src/main/groovy/no/skatteetaten/aurora/gradle/plugins/docker/AuroraDockerPlugin.groovy
@@ -12,23 +12,24 @@ class AuroraDockerPlugin implements Plugin<Project> {
     project.with {
       extensions.create('auroradocker', AuroraDockerPluginExtension)
 
-      task('buildImage') << {
+      task('buildImage') {
+        doLast {
+          String workDir = new File(project.auroradocker.workingDir)
 
-        String workDir = new File(project.auroradocker.workingDir)
+          String imageTag = "$auroradocker.imageName:${version}"
+          String buildCmd = DockerCommands.createBuildCommand(imageTag, auroradocker.buildArgs)
 
-        String imageTag = "$auroradocker.imageName:${version}"
-        String buildCmd = DockerCommands.createBuildCommand(imageTag, auroradocker.buildArgs)
+          ProcessTools.Result result = ProcessTools.runCommand(buildCmd, workDir)
 
-        ProcessTools.Result result = ProcessTools.runCommand(buildCmd, workDir)
-
-        if (result.process.exitValue() != 0) {
-          throw new GradleException("An error occurred while building the image. Inspect output for more details.")
+          if (result.process.exitValue() != 0) {
+            throw new GradleException("An error occurred while building the image. Inspect output for more details.")
+          }
+          String imageId = DockerTools.getImageIdFromOutput(result.output)
+          if (!imageId) {
+            throw new GradleException("Unable to find id of built image in output.")
+          }
+          ext.imageId = imageId
         }
-        String imageId = DockerTools.getImageIdFromOutput(result.output)
-        if (!imageId) {
-          throw new GradleException("Unable to find id of built image in output.")
-        }
-        ext.imageId = imageId
       }
 
       task('tagImage') {
@@ -38,7 +39,8 @@ class AuroraDockerPlugin implements Plugin<Project> {
           String imageNameWithRegistry = "$auroradocker.registry/$auroradocker.imageName"
           // If the revision property has been set, we include a tag with it included.
           // The aurora plugin, by default, sets the revision property to the current git hash.
-          Set<String> versions = DockerTagTools.createVersionTagsFromVersionAndRevision(project.version, project.revision)
+          Set<String> versions = DockerTagTools.
+              createVersionTagsFromVersionAndRevision(project.version, project.revision)
           List<String> tags = versions.collect { "$imageNameWithRegistry:$it" }
 
           tags.each { tag ->
@@ -51,7 +53,8 @@ class AuroraDockerPlugin implements Plugin<Project> {
         dependsOn tagImage
         doLast {
           String imageNameWithRegistry = "$auroradocker.registry/$auroradocker.imageName"
-          Set<String> versions = DockerTagTools.createVersionTagsFromVersionAndRevision(project.version, project.revision)
+          Set<String> versions = DockerTagTools.
+              createVersionTagsFromVersionAndRevision(project.version, project.revision)
           List<String> tags = versions.collect { "$imageNameWithRegistry:$it" }
           tags.each { tag ->
             ProcessTools.runCommand("docker push $tag")
